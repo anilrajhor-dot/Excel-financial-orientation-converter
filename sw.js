f14ce7a41f79632d8b8f4f020a89bedff1185a5c
anilrajhor-dot <anilrajhor@gmail.com>
2026-09-02 08:04:03 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-const CACHE='efoc-v6';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(clients.claim()));self.addEventListener('fetch',e=>{});
+const CACHE="excel-converter-v7";
+const CORE=["./","./index.html","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
+  }).catch(()=>caches.match("./index.html"))));
+});
