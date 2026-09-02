4872667b22100948de41a297371cc9455840111c
anilrajhor-dot <anilrajhor@gmail.com>
2026-09-02 08:38:05 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE="excel-converter-v7";
-const CORE=["./","./index.html","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+const CACHE="excel-financial-converter-v8";
+const ASSETS=["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET")return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
-  }).catch(()=>caches.match("./index.html"))));
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{
+    if(new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}
+    return r;
+  }).catch(()=>cached)));
 });
